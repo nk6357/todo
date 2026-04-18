303207637d457a741c2a40025e878e69b87a7cc8
nk6357 <walkinglion321@gmail.com>
2026-04-18 14:27:35 +0000

    update: pack24 catalog 2026-04-18

---
+++ sw.js
@@ -0,0 +1,26 @@
+// sw.js
+
+// Кэшируем важные файлы, чтобы приложение работало даже без интернета
+const CACHE_NAME = 'ege-app-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './russian.html',
+  './math.html',
+  './informatics.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
